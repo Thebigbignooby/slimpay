ab39a8f9b11f565b6a456e8a928c9e11c9894380
Rory Dent <whishana@gmail.com>
2015-09-29 19:45:14 +0200

    pretranspiled module to avoid babel polyfill conflict

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require("babel/polyfill");
-require('babel/register')({
-	stage: 0,
-	experimental: true
-});
+// require("babel/polyfill");
+// require('babel/register')({
+// 	stage: 0,
+// 	experimental: true
+// });
 
-module.exports = require('./src/slimpay');
+module.exports = require('./lib/slimpay');
